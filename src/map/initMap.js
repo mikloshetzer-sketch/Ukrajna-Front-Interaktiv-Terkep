c39581a59be587488786f9c82ce8c801bd50a2aa
mikloshetzer-sketch <mikloshetzer@gmail.com>
2026-07-07 10:04:49 +0200

    Update initMap.js

--- src/map/initMap.js
+++ src/map/initMap.js
@@ -1,26 +1,49 @@
 export function initMap() {
   const map = L.map('map', { zoomControl: true }).setView([48.5, 33.5], 6);
 
-  const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; OpenStreetMap'
-  }).addTo(map);
+  const baseLayers = {
+    osm: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; OpenStreetMap'
+    }),
 
-  const carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-    maxZoom: 20,
-    attribution: '&copy; OpenStreetMap &copy; CARTO'
-  });
+    carto: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+      maxZoom: 20,
+      attribution: '&copy; OpenStreetMap &copy; CARTO'
+    }),
 
-  const esriSat = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-    maxZoom: 19,
-    attribution: 'Tiles &copy; Esri'
-  });
+    esri: L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        maxZoom: 19,
+        attribution: 'Tiles &copy; Esri'
+      }
+    )
+  };
 
-  L.control.layers(
-    { 'OpenStreetMap': osm, 'CARTO Light': carto, 'Műholdkép (Esri)': esriSat },
-    {},
-    { collapsed: false }
-  ).addTo(map);
+  let activeBaseLayerKey = 'osm';
+  baseLayers.osm.addTo(map);
+
+  map.baseLayers = baseLayers;
+
+  map.getActiveBaseLayerKey = function () {
+    return activeBaseLayerKey;
+  };
+
+  map.setBaseLayer = function (layerKey) {
+    const nextLayerKey = baseLayers[layerKey] ? layerKey : 'osm';
+
+    Object.values(baseLayers).forEach(layer => {
+      if (map.hasLayer(layer)) {
+        map.removeLayer(layer);
+      }
+    });
+
+    baseLayers[nextLayerKey].addTo(map);
+    activeBaseLayerKey = nextLayerKey;
+
+    return activeBaseLayerKey;
+  };
 
   return map;
 }
